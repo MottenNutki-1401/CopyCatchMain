98f5898a2d6185b128dedcea8c7f86cbdf42bdd9
Tina <202310991@gordoncollege.edu.ph>
2026-03-06 13:39:16 +0800

    partial start for frontend of copycatch

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+// i organized everything as whole
+import Logins from "./pages/logins.jsx";
+
+function App() {
+  return (
+    <>
+      <Logins />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/logins.jsx
@@ -0,0 +1,40 @@
+import "./style/login.css";
+
+function Logins() {
+  return (
+    <div className="login-container">
+      <h1>Hello there!</h1>
+
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+            <button type="submit">Login </button>
+        </form>
+    </div>
+  );
+}
+
+export default Logins;
+
+
+
+
+
+
+
+
+//function login() {
+ // return (
+ //   <div className="login-container">
+  //    <h2>Loginimsssms</h2>
+
+   //   <form>
+   //     <input type="text" placeholder="Username" />
+
+   //     <input type="password" placeholder="Password" />
+
+    //    <button type="submit">Login</button>
+// </div>
+// )
+//}
+//export default login;
